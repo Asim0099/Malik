b46e9cb0029bfd6fc0efc127504309bdfd927e18
Asim0099 <98441505+Asim0099@users.noreply.github.com>
2022-01-26 09:49:18 +0500

    Create Malik.py
    https://github.com/Asim0099/Malik.git
    cd Blueforce--Asim
    python2 ab.sys

---
+++ Malik.py
@@ -0,0 +1,80 @@
+<p align="center">
+
+<a href="https://bit.ly/3jLqF1P"><img title="Made in Pakistan" src="https://img.shields.io/badge/MADE%20IN-Pakistan-SCRIPT?colorA=%23ff8100&colorB=%23017e40&colorC=%23ff0000&style=for-the-badge"></a>
+
+</p>
+
+<p align="center">
+
+<a href="https://bit.ly/3jLqF1P"><img title="Made in Pakistan" src="https://img.shields.io/badge/Tool-Blurforce--Abm-green.svg"></a>
+
+<a href="https://bit.ly/3jLqF1P"><img title="Version" src="https://img.shields.io/badge/Version-3.0-green.svg?style=flat-square"></a>
+
+<a href="https://bit.ly/3jLqF1P"><img title="Cloning" src="https://img.shields.io/badge/Cloning%3F-yes-green.svg"></a>
+
+![king](https://user-images.githubusercontent.com/52023076/108603102-cf790d80-735a-11eb-8659-c165399b1998.jpg)
+
+# Blurforce--asim
+
+```
+
+pkg update && upgrade
+
+pkg install git
+
+pkg install python2
+
+pip2 install requestshttps://github.com/Asim0099/Asim.git
+
+pip2 install mechanize
+
+pip2 install requests bs4
+
+rm -rf Blueforce--asim
+
+git clonehttps://github.com/Asim0099/Malik.git
+cd Blueforce--Asim
+
+
+python2 ab.sys
+
+```
+
+# Tool User And Pasa
+
+```
+
+Username : Asim
+
+Password : Malik
+
+```
+
+# Avaible on
+
+```
+
+termux
+
+linux
+
+```
+
+# Note
+
+```
+
+If you copy my scripts or edit my scripts,so i will fuck your ass
+
+So do not copy my scripts
+
+```
+
+# Author
+
+```
+
+Alone codder : Asim Malik
+
+```
+
